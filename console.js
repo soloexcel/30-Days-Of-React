790d466130dc40390e7f3afee30bec81477f3efd
soloexcel <solomonlekan1@gmail.com>
2022-11-13 03:07:15 -0500

    2nd day of react

---
+++ console.js
@@ -0,0 +1,11 @@
+// The practice on various examples here
+
+/*
+const jsxElement = <h1>I am a Jsx element</h1>
+const welcome = <h1>welcome to 30 days of react challenge</h1>
+const data = <small>Nov 13, 2022</small>
+const title = <h1>I am a jsx element</h1>
+
+console.log(data)
+*/
+
